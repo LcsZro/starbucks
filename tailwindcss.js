98617a17dcb03efa86fe8095f7520a7c2cfbb62a
Lucas <86057306+LcsZro@users.noreply.github.com>
2022-09-07 11:33:24 -0300

    Initial commit

---
+++ tailwindcss.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+        starbucks: '#D4E9E2',
+        letterstar: '#1E3932',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
